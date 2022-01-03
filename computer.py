aec6ba4d770dcb65052dd3d2c6a7a2bbbfe7eecc
burjuly <you@example.com>
2022-01-04 00:26:31 +0300

    3j

---
+++ computer.py
@@ -0,0 +1,13 @@
+import re
+
+
+def main():
+    while True:
+        print(f'>>>', end='')
+        instruction = input()
+
+        if instruction == 'exit':
+            break
+
+if __name__== "__main__":
+    main()
